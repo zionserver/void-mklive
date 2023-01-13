36087ae3ad790f5dc62fa7f4fa06c7938c42898b
gudrak <63885769+zionserver@users.noreply.github.com>
2023-01-13 06:30:39 -0300

    Update locale.sh

--- dracut/vmklive/locale.sh
+++ dracut/vmklive/locale.sh
@@ -5,7 +5,7 @@
 type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
 
 LOCALE=$(getarg locale.LANG)
-[ -z "$LOCALE" ] && LOCALE="en_US.UTF-8"
+[ -z "$LOCALE" ] && LOCALE="es_AR.UTF-8"
 
 # also enable this locale in newroot.
 echo "LANG=$LOCALE" > $NEWROOT/etc/locale.conf
@@ -13,7 +13,7 @@ echo "LC_COLLATE=C" >> $NEWROOT/etc/locale.conf
 
 # set keymap too.
 KEYMAP=$(getarg vconsole.keymap)
-[ -z "$KEYMAP" ] && KEYMAP="us"
+[ -z "$KEYMAP" ] && KEYMAP="es"
 
 if [ -f ${NEWROOT}/etc/vconsole.conf ]; then
     sed -e "s,^KEYMAP=.*,KEYMAP=$KEYMAP," -i $NEWROOT/etc/vconsole.conf
